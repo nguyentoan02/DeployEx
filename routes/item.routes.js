7db53c9f19152f1eddb7c5629069165821619c68
ToanNV <144198300+nguyentoan02@users.noreply.github.com>
2025-03-08 11:07:17 +0700

    Update item.routes.js

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/item.controller');
-
+//lo
 router.post('/items', itemController.createItem);
 router.get('/items', itemController.getItems);
 router.get('/items/:id', itemController.getItemById);
